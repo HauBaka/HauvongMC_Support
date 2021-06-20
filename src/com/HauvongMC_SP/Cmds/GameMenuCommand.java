d3baf52e8403fa8ef60c5e069cb2bb9c08c2c5e6
HauvongMC <tuquynh7381@gmail.com>
2021-06-20 21:10:34 +0700

    output lol
    asd

--- src/com/HauvongMC_SP/Cmds/GameMenuCommand.java
+++ src/com/HauvongMC_SP/Cmds/GameMenuCommand.java
@@ -5,7 +5,6 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-
 public class GameMenuCommand implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
