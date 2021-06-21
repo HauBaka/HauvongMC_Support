26c201bd05477469d8759099bcc31db17e141bc5
HauvongMC <tuquynh7381@gmail.com>
2021-06-21 14:46:24 +0700

    lol death crys v10

--- src/com/HauvongMC_SP/BedWars/DeathCry.java
+++ src/com/HauvongMC_SP/BedWars/DeathCry.java
@@ -3,13 +3,19 @@ package com.HauvongMC_SP.BedWars;
 import com.HauvongMC_SP.Main;
 import com.andrei1058.bedwars.api.events.player.PlayerKillEvent;
 import com.andrei1058.bedwars.api.events.player.PlayerReSpawnEvent;
+import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
 
 public class DeathCry implements Listener {
     @EventHandler
-    public void playerKill(PlayerReSpawnEvent event) {
-        PlayDeathCrySound.play(event.getPlayer(), Main.deathcrysdata.getString("Players." + event.getPlayer().getName() + ".Current_Select"));
+    public void playerKill(PlayerKillEvent event) {
+        Bukkit.getScheduler().scheduleSyncDelayedTask(Main.getPlugin(), new Runnable() {
+            @Override
+            public void run() {
+                PlayDeathCrySound.play(event.getVictim().getPlayer(), Main.deathcrysdata.getString("Players." + event.getVictim().getPlayer().getName() + ".Current_Select"));
+            }
+        }, 5);
     }
 }
